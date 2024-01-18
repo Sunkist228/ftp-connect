a04c85196cc45d22420a63311aba67d323fa3972
Sunkist <145262380+Sunkist228@users.noreply.github.com>
2024-01-18 16:37:49 +0300

    modified

--- main.py
+++ main.py
@@ -11,40 +11,42 @@ def read_exclude_list(filename):
     with open(filename, 'r') as f:
         return [line.strip() for line in f]
 
-def upload_files_ftp(server, username, password, local_dir, remote_dir, exclude_list):
-    # Установка соединения с FTP-сервером
-    ftp = FTP(server)
+def upload_files_ftp(server, port, username, password, local_dir, remote_dir, exclude_list):
+    # Establish connection to the FTP server
+    ftp = FTP()
+    ftp.connect(server, port)
     ftp.login(username, password)
 
-    # Переход в удаленный каталог
+    # Change to the remote directory
     ftp.cwd(remote_dir)
 
-    # Получение списка файлов в локальном каталоге
+    # Get the list of files in the local directory
     local_files = os.listdir(local_dir)
 
-    # Фильтрация файлов с учетом исключений
+    # Filter files based on exclusions
     files_to_upload = [file for file in local_files if file not in exclude_list]
 
-    # Отправка файлов на FTP-сервер
+    # Upload files to the FTP server
     for file in files_to_upload:
         with open(os.path.join(local_dir, file), 'rb') as f:
             ftp.storbinary('STOR ' + file, f)
 
-    # Закрытие соединения
+    # Close the connection
     ftp.quit()
 
-# Чтение конфигурации из файла config.ini
+# Read configuration from config.ini file
 config = read_config()
 
-# Чтение списка исключений из файла
+# Read the list of exclusions from the file
 exclude_list_file = config['exclude_list_file']
 exclude_list = read_exclude_list(exclude_list_file)
 
-# Пример использования
+# Example usage
 server = config['server']
+port = int(config['port'])
 username = config['username']
 password = config['password']
 local_directory = config['local_directory']
 remote_directory = config['remote_directory']
 
-upload_files_ftp(server, username, password, local_directory, remote_directory, exclude_list)
+upload_files_ftp(server, port, username, password, local_directory, remote_directory, exclude_list)
